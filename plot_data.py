d68b3340b65a9818301f6497ba71bf419d79b722
slt <ge75wed@mytum.de>
2020-01-31 17:40:48 +0100

    version 2.9

--- plot_data.py
+++ plot_data.py
@@ -31,7 +31,7 @@ def hist_data(test_predict, db_predict, db_label):
     arg20 = 0
     arg40 = 0
     arg180 = 0
-    predict_label = np.zeros((test_label.shape[0],5))
+    predict_label = np.zeros((test_predict.shape[0],5))
     for i in range(test_predict.shape[0]):
         dist = test_predict[i] - db_predict
         predict_label[i] = db_label[np.argmin(np.max(dist ** 2,axis=1))]
